c02778a0bdd990ba6ba261c22a7d41833bf93f8f
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-10 16:48:39 +0200

    avatar added

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { PlayerWrapper } from './Player.styled'
+import Avatar, { genConfig } from 'react-nice-avatar'
 
-const Player = ({player, isPlayerActive}) => {
+
+const Player = ({ player, isPlayerActive }) => {
+  const config = genConfig();
+
   return (
     <PlayerWrapper isPlayerActive={isPlayerActive ?? false}>
+      <Avatar style={{ width: '8rem', height: '8rem' }} {...config} />
       <div>{`${player.name}(${player.choice})`}</div>
       <div>{player.score}</div>
     </PlayerWrapper>
